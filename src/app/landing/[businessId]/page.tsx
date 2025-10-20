fa11f5fdb65312391e7ed70bf77d05341fb868f2
alex jf <allseosoporte@gmail.com>
2025-10-20 20:05:03 +0000

    mira la imagen salio este error.

--- src/app/landing/[businessId]/page.tsx
+++ src/app/landing/[businessId]/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useParams } from 'next/navigation';
 import { useFirestore, useDoc, useMemoFirebase } from '@/firebase';
 import { doc } from 'firebase/firestore';
